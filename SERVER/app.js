718c0edcf9c13f8f0559120409aa1e41dade57e5
aisehiwebwala <backw777@gmail.com>
2025-08-23 17:41:55 +0530

    updated

--- SERVER/app.js
+++ SERVER/app.js
@@ -2,10 +2,15 @@ const express = require('express')
 const app = express()
 const cors = require("cors")
 const api_router = require("./api")
+const path = require("path")
 
 app.use(cors())
 app.use(express.json())
 app.use("/api",api_router);
-app.get('/', (req, res) => res.send('Hello Dunia!'))
+
+app.use(express.static(path.join(__dirname,"../CLIENT/dist")))
+app.get("/{*any}",(req,res)=>{
+    return res.sendFile(path.join(__dirname,"../CLIENT/dist/index.html"))
+})
 
 module.exports = app;
