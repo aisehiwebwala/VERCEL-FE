11298ca966927be70b01e8155d145674411dd1a0
aisehiwebwala <backw777@gmail.com>
2025-08-24 12:42:08 +0530

    dotenv

--- CLIENT/src/Header.jsx
+++ CLIENT/src/Header.jsx
@@ -3,6 +3,8 @@ import {Link, Links} from "react-router-dom"
 function Header() {
   return (
     <div className='header' style={{display:"flex",gap:"10px",borderBottom:"2px solid black",padding:"5px"}} >
+        ENV : {import.meta.env.VITE_ENV}
+        CORS : {import.meta.env.CORS_ENABLED}
         <Link to={"/"} > Home </Link>
         <Link to={"/about"} > About </Link>
         <Link to={"/ops"} > Operations </Link>
--- SERVER/app.js
+++ SERVER/app.js
@@ -1,16 +1,23 @@
 const express = require('express')
 const app = express()
 const cors = require("cors")
+const dotenv = require("dotenv")
+dotenv.config()
+
 const api_router = require("./api")
 const path = require("path")
 
-app.use(cors())
-app.use(express.json())
-app.use("/api",api_router);
+if (process.env.CORS_ENABLED === "true") {
+    console.log("CORS ENABLED")
+    app.use(cors())
+}
 
-app.use(express.static(path.join(__dirname,"../CLIENT/dist")))
-app.get("/{*any}",(req,res)=>{
-    return res.sendFile(path.join(__dirname,"../CLIENT/dist/index.html"))
+app.use(express.json())
+app.use("/api", api_router);
+
+app.use(express.static(path.join(__dirname, "../CLIENT/dist")))
+app.get("/{*any}", (req, res) => {
+    return res.sendFile(path.join(__dirname, "../CLIENT/dist/index.html"))
 })
 
 module.exports = app;
